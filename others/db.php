8a73695166a879a4fe7578d06875e4843a6fee45
Johanu <johanugandonou@gmail.com>
2024-08-23 09:38:36 +0000

    Changement des informations de base de donnée

--- others/db.php
+++ others/db.php
@@ -1,7 +1,7 @@
 <?php
     //Connexion à la bd
     try{
-        $bdd = new PDO("mysql:host=localhost;dbname=cours_en_ligne;charset=utf8","root","", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        $bdd = new PDO("mysql:host=localhost;dbname=tqffjfwm_oclass;charset=utf8","tqffjfwm_oclass",'o3tJ$cA#1:to$tJotc', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     }
     catch(Exception $e){
         die('Erreur : '.$e->getMessage());
